4571dcbbf2977cbc3153ceac4f887cf1cec758b0
jakubmraz <58209778+jakubmraz@users.noreply.github.com>
2021-10-01 13:02:34 +0200

    The project

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public List<ItemSlot> ItemSlots;
+
+    public ItemSlot FindFirstEmptySlot()
+    {
+        foreach (var itemSlot in ItemSlots)
+        {
+            if (!itemSlot.Item)
+                return itemSlot;
+        }
+
+        return null;
+    }
+}
---
+++ Assets/Scripts/ItemSlot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemSlot : MonoBehaviour
+{
+    public Item itemPrefab;
+
+    public string debugItem;
+    public Item Item;
+
+    public bool IsCraftingSlot;
+    public bool IsResultSlot;
+
+    void Awake()
+    {
+        if (debugItem != string.Empty)
+        {
+            Item = Instantiate(itemPrefab, transform).GetComponent<Item>();
+            Item.InitializeItem(debugItem);
+        }
+    }
+}
